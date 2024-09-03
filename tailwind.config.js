38024fcfc62da64ff077074e893eb425b1065c48
JabssDev <jordanburgoss23@gmail.com>
2024-09-03 05:32:10 -0500

    style: :art: Add custom colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				ct: {
+					green: "hsl(75, 94%, 57%)",
+					"grey-700": "hsl(0, 0%, 20%)",
+					"grey-800": "hsl(0, 0%, 12%)",
+					"grey-900": "hsl(0, 0%, 8%)",
+				},
+			},
+			fontFamily: {
+				sans: ["Inter", "sans-serif"],
+			},
+		},
 	},
 	plugins: [],
 };
